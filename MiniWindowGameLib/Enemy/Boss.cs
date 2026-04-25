6d0265c48b927afd13b4bf2ac2912f18a9cd9cc4
Damian Woźny <damianmdxvpc18@gmail.com>
2026-04-25 02:32:47 +0200

    bug fix in boss class

--- MiniWindowGameLib/Enemy/Boss.cs
+++ MiniWindowGameLib/Enemy/Boss.cs
@@ -7,7 +7,7 @@ namespace MiniWindowGameLib.Enemy
     {
         public Skill SpecialAttack { get; set; }
 
-        public Boss(string name, int level, int maxHp, int baseDamage, int str, int dex, int _int, int reduction, , int exp, int gold, Skill specialAttack) :
+        public Boss(string name, int level, int maxHp, int baseDamage, int str, int dex, int _int, int reduction, int exp, int gold, Skill specialAttack) :
             base(name, level, maxHp, baseDamage, str, dex, _int, reduction, exp, gold)
         {
             SpecialAttack = specialAttack;
